14fdbf1f328f736bd879d0b2288f6a918ad9f8bc
manish.pandey <manish.pandey@STPL-INTERN-MAN>
2023-04-06 13:08:27 +0545

    Add project files.

---
+++ MeetingRoomBooking/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using MeetingRoomBooking.Models;
+using Microsoft.AspNetCore.Mvc;
+using Services.Hall;
+using System.Diagnostics;
+
+namespace MeetingRoomBooking.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHallService _hallService;
+
+        public HomeController(IHallService hallService)
+        {
+            _hallService = hallService;
+        }
+
+        public IActionResult Index()
+        {
+             var name= _hallService.GetName();
+
+            ViewBag.name = name;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+    }
+}
---
+++ Services/Hall/HallService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Hall
+{
+    public class HallService: IHallService
+    {
+        public string GetName()
+        {
+            return "test";
+        }
+        //list
+        //create
+        //edit?id=12
+        //detaoilView?id=12
+    }
+}
